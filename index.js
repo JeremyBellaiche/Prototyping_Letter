2beb37fa75804ddc7e40b9b32593859e1e2155ec
Jeremy Bellaiche <jeremy.bellaiche@MacBook-Pro.local>
2016-02-24 11:40:51 +0100

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var users = [];
+var path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
